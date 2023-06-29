92255f4362bb20c09a8fcb4e2060adc34a28bb26
Farith77 <120695805+Farith77@users.noreply.github.com>
2023-06-29 13:19:53 -0500

    Latex

---
+++ Latex/src/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+'''def home(request):
+    title = 'Navin'
+    return render(request, 'home.html', {
+        'title': title
+    })
+    
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
+    return render(request, 'result.html',{
+        'result': res
+    })'''
+
+def index(request):
+    return render(request, 'index.html')
